8b42e9a5a84f50ac6b2625aa46280735f0ae0ef6
rchopinw <rchopin@outlook.com>
2022-04-27 13:03:22 -0400

    add weights and model saving methods

--- TensorFlowTools.py
+++ TensorFlowTools.py
@@ -4,7 +4,7 @@ import os
 
 
 class ModelCheckPointSaver(tf.keras.callbacks.Callback):
-    def __init__(self, model_id, checkpoint_dir, max_num_weights=5):
+    def __init__(self, model_id, checkpoint_dir, max_num_weights=5, save_weights=True):
         super().__init__()
 
         self.checkpoint_dir = checkpoint_dir
@@ -14,17 +14,26 @@ class ModelCheckPointSaver(tf.keras.callbacks.Callback):
 
         self.max_num_weights = max_num_weights
         self.model_id = model_id
+        self.save_weights = save_weights
 
     def on_epoch_end(self, epoch, logs=None):
         min_acc_file, max_acc_file, max_acc, num_weights = self.scan_weight_files()
         cur_acc = logs["val_sparse_categorical_accuracy"]
         if cur_acc > max_acc:
-            save_name = "weights.e{}-acc{}.h5".format(
-                epoch, cur_acc
-            )
-            self.model.save_weights(
-                self.checkpoint_dir + os.sep + self.model_id + "." + save_name
-            )
+            if self.save_weights:
+                save_name = "weights.e{}-acc{}.h5".format(
+                    epoch, cur_acc
+                )
+                self.model.save_weights(
+                    self.checkpoint_dir + os.sep + self.model_id + "." + save_name
+                )
+            else:
+                save_name = "model.e{}-acc{}.h5".format(
+                    epoch, cur_acc
+                )
+                self.model.save(
+                    self.checkpoint_dir + os.sep + self.model_id + "." + save_name
+                )
             if self.max_num_weights and num_weights + 1 > self.max_num_weights:
                 os.remove(self.checkpoint_dir + os.sep + min_acc_file)
 
