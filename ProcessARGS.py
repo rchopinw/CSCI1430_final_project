297920b8b7fa3127295d5cb76d47c756d4117133
rchopinw <rchopin@outlook.com>
2022-05-04 18:29:38 -0400

    finalized

--- ProcessARGS.py
+++ ProcessARGS.py
@@ -6,10 +6,10 @@ class GlobalArgs:
     random_seed = 42
     validation_split = 0.2
     num_classes = 3
-    log_dir = "/home/bangxi_xiao2/train_output/log"
-    model_dir = "/home/bangxi_xiao2/train_output/model"
-    face_recognition_dnn_dir = '/home/bangxi_xiao2/res10_300x300_ssd_iter_140000.caffemodel'
-    face_recognition_dnn_prototxt_dir = '/home/bangxi_xiao2/deploy.prototxt.txt'
+    log_dir = "../train_output/log"
+    model_dir = "../train_output/model"
+    face_recognition_dnn_dir = '../face_recognition_model/res10_300x300_ssd_iter_140000.caffemodel'
+    face_recognition_dnn_prototxt_dir = '../face_recognition_model/deploy.prototxt.txt'
     # log_dir = "D:/Brown Learning Materials/CSCI1430/train_logs/log"
     # model_dir = "D:/Brown Learning Materials/CSCI1430/train_logs/model"
     # face_recognition_dnn_dir = \
@@ -19,11 +19,11 @@ class GlobalArgs:
 
 
 class TFRecordConfigArgs:
-    train_directory = '/home/bangxi_xiao2/data/images'
-    train_save_directory = '/home/bangxi_xiao2/data'
+    train_directory = '../data/train_images'  # original images are no longer saved
+    train_save_directory = '../data/train_validation_data'
     train_size = 5000
-    test_directory = '/home/bangxi_xiao2/data/images'
-    test_save_directory = '/home/bangxi_xiao2/data/test_tfrecords'
+    test_directory = '../data/test_images'  # original images are no longer saved
+    test_save_directory = '../data/test_data'
     # test_directory = "D:/Brown Learning Materials/CSCI1430/archive/images"
     # test_save_directory = "D:/Brown Learning Materials/CSCI1430/archive/test_tfrecords"
     test_size = 1000
